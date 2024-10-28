d3b0aaaeecf100a22fd741efc6059e950314edf9
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2024-10-28 03:21:14 +0000

    fix: proxy not working (Production Only)
    - If you need to test this locally, go to de "dev" branch and run the code there. This one is not going to work on localhost because of SSL certification

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const https = require('https');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/api/*', async (req, res) => {
+app.use(express.json()); // To parse JSON bodies
+
+app.all('/api/*', async (req, res) => {
   const path = req.params[0]; // Get the path after /api/
   const query = req.query; // Get the query parameters
-  const targetUrl = `https://dev.same.com.co/${path}`;
+  const targetUrl = `https://dev.same.com.co/api/public/${path}`;
+
+  // Retrieve headers from the incoming request
+  const incomingHeaders = req.headers;
+
+  // Set up headers for the target request
+  const headers = {
+    ...incomingHeaders,
+    'apiKey': process.env.VITE_API_KEY,
+    'secretKey': process.env.VITE_SECRET_KEY,
+  };
 
   try {
-    const response = await axios.get(targetUrl, {
-      headers: {
-        'apiKey': process.env.VITE_API_KEY,
-        'secretKey': process.env.VITE_SECRET_KEY,
-      },
+    const response = await axios({
+      method: req.method,
+      url: targetUrl,
+      headers: headers,
       params: query,
+      data: req.body, // Forward the request body if present
     });
-
-    res.json(response.data);
+    res.status(response.status).json(response.data);
   } catch (error) {
     res.status(error.response ? error.response.status : 500).send(error.message);
   }
