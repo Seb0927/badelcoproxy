be57be9fb4afcbaab63f3fab6d6576ddaa55d9bd
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2024-10-28 03:19:20 +0000

    fix: use only for testing development.
    - The reason behind this is because, this code is not doing SSL certification. This is useful when we are running our proxy server in localhost, but this code being deployed to production could cause securitty issues since Man-In-The-Middle attack can be done without SSL protecting.

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const https = require('https');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/api/*', async (req, res) => {
+app.use(express.json()); // To parse JSON bodies
+
+// Create an HTTPS agent that disables SSL certificate validation
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
+app.all('/api/*', async (req, res) => {
   const path = req.params[0]; // Get the path after /api/
   const query = req.query; // Get the query parameters
-  const targetUrl = `https://dev.same.com.co/${path}`;
+  const targetUrl = `https://dev.same.com.co/api/public/${path}`;
+
+  // Retrieve headers from the incoming request
+  const incomingHeaders = req.headers;
+
+  // Set up headers for the target request
+  const headers = {
+    ...incomingHeaders,
+    'apiKey': process.env.VITE_API_KEY,
+    'secretKey': process.env.VITE_SECRET_KEY,
+  };
 
   try {
-    const response = await axios.get(targetUrl, {
-      headers: {
-        'apiKey': process.env.VITE_API_KEY,
-        'secretKey': process.env.VITE_SECRET_KEY,
-      },
+    const response = await axios({
+      method: req.method,
+      url: targetUrl,
+      headers: headers,
       params: query,
+      data: req.body, // Forward the request body if present
+      httpsAgent: httpsAgent, // Use the HTTPS agent that disables SSL certificate validation
     });
 
-    res.json(response.data);
+    res.status(response.status).json(response.data);
   } catch (error) {
     res.status(error.response ? error.response.status : 500).send(error.message);
   }
